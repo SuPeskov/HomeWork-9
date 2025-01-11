0a6381ab9e9d66da66e9e6ec47e750091505bde7
SuPeskov <supeskov@yandex.ru>
2025-01-11 14:39:07 +0300

    выполнена базоая настройка eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,16 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+{
+  ignores: ['node_moules', 'dist']
+},
+{
+  languageOptions: {
+    globals: {
+     ...globals.browser,
+     ...globals.es2025
+    }
+  }
+},
   pluginJs.configs.recommended,
 ];
