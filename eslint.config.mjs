928b8331ec75e77211bf74500512f2dab1fb0421
SuPeskov <supeskov@yandex.ru>
2025-01-11 22:01:18 +0300

    выполнен запуск npm run

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,30 +3,29 @@ import pluginJs from '@eslint/js';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-{
-  ignores: ['node_moules', 'dist']
-},
-{
-  languageOptions: {
-    globals: {
-     ...globals.browser,
-     ...globals.es2025
-    }
-  }
-},
+  {
+    ignores: ['node_moules', 'dist'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2025,
+      },
+    },
+  },
   pluginJs.configs.recommended,
   {
     files: ['**/*.js'],
     plugins: {
-      prittier: eslintPluginPrettier
+      prittier: eslintPluginPrettier,
     },
     rules: {
       ...eslintConfigPrettier.rules,
       'arrow-body-style': 'off',
       'prefer-arrow-callback': 'off',
-    }
-  }
+    },
+  },
 ];
